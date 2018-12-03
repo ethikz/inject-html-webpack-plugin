b298fb72e1d163426e5e372d3ce7c199655b4d22
Christensen, Josh <JChristensen@BBandT.com>
2018-12-03 15:43:39 -0500

    chore(lint): convert linting rc files to js

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+const path = require( 'path' );
+
+module.exports = {
+  extends: [
+    '@ethikz/eslint-config'
+  ]
+}
---
+++ .stylelintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: [
+    '@ethikz/stylelint-config'
+  ]
+};
